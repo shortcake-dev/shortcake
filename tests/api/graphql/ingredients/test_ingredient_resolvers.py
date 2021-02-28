b5632fe91e4f5e2a1882bcaacc3f703db8736ae1
ignormies <bryce.beagle@gmail.com>
2021-02-28 12:36:25 -0800

    Add unit tests for strawberry queries

---
+++ tests/api/graphql/ingredients/test_ingredient_resolvers.py
@@ -0,0 +1,56 @@
+from uuid import UUID
+
+from shortcake.api.graphql import schema
+
+from tests.utils import fake_db
+
+
+class TestIngredientResolver:
+    def test_fields(self):
+        query = """
+            query($id: ID!) {
+                ingredient(id_: $id) {
+                    id,
+                    name
+                }
+            }
+        """
+
+        db_ingredient = fake_db.Ingredient()
+
+        result = schema.execute_sync(
+            query=query, variable_values={"id": str(db_ingredient.id)}
+        )
+
+        assert not result.errors
+        ingredient = result.data["ingredient"]
+
+        assert UUID(ingredient["id"]) == db_ingredient.id
+        assert ingredient["name"] == db_ingredient.name
+
+
+class TestIngredientsResolver:
+    def test_ids(self):
+        query = """
+            {
+                ingredients {
+                    id
+                }    
+            }
+        """
+
+        db_ingredients = [
+            fake_db.Ingredient(),
+            fake_db.Ingredient(),
+            fake_db.Ingredient(),
+        ]
+
+        result = schema.execute_sync(query)
+        assert not result.errors
+
+        graphql_ingredient_ids = [
+            UUID(ingredient["id"]) for ingredient in result.data["ingredients"]
+        ]
+        db_ingredient_ids = [ingredient.id for ingredient in db_ingredients]
+
+        assert graphql_ingredient_ids == db_ingredient_ids
---
+++ tests/api/graphql/recipes/test_recipe_resolvers.py
@@ -0,0 +1,60 @@
+from uuid import UUID
+
+from shortcake.api.graphql import schema
+
+from tests.utils import fake_db
+
+
+class TestRecipeResolver:
+    def test_fields(self):
+        query = """
+            query($id: ID!) {
+                recipe(id_: $id) {
+                    id,
+                    name,
+                    description,
+                    ingredients {
+                        id
+                    }
+                }
+            }
+        """
+
+        db_recipe = fake_db.Recipe()
+        db_ingredient = fake_db.Ingredient()
+        _db_recipe_ingredient = fake_db.RecipeIngredient(
+            recipe=db_recipe, ingredient=db_ingredient
+        )
+
+        result = schema.execute_sync(
+            query=query, variable_values={"id": str(db_recipe.id)}
+        )
+
+        assert not result.errors
+        recipe = result.data["recipe"]
+
+        assert UUID(recipe["id"]) == db_recipe.id
+        assert recipe["name"] == db_recipe.name
+        assert recipe["description"] == db_recipe.description
+        assert UUID(recipe["ingredients"][0]["id"]) == db_ingredient.id
+
+
+class TestRecipesResolver:
+    def test_ids(self):
+        query = """
+            {
+                recipes {
+                    id
+                }    
+            }
+        """
+
+        db_recipes = [fake_db.Recipe(), fake_db.Recipe(), fake_db.Recipe()]
+
+        result = schema.execute_sync(query)
+        assert not result.errors
+
+        graphql_recipe_ids = [UUID(recipe["id"]) for recipe in result.data["recipes"]]
+        db_recipe_ids = [recipe.id for recipe in db_recipes]
+
+        assert graphql_recipe_ids == db_recipe_ids
