e51ceb09179a52bc27688a2c4570c4938472a80f
ignormies <bryce.beagle@gmail.com>
2021-04-02 05:48:58 -0700

    Configure CORS to allow localhost domains during debug

---
+++ shortcake/asgi/app.py
@@ -0,0 +1,28 @@
+from starlette.applications import Starlette
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+from starlette.routing import Route, WebSocketRoute
+from strawberry.asgi import GraphQL
+
+from shortcake.api.graphql import schema as graphql_schema
+
+
+class BrainFrameApp(Starlette):
+    def __init__(self, debug: bool = False):
+        self.graphql_app = GraphQL(graphql_schema, debug=debug)
+
+        routes = [
+            Route("/", self.graphql_app),
+            WebSocketRoute("/", self.graphql_app),
+        ]
+
+        middleware = []
+        if debug:
+            cors_middleware = Middleware(
+                CORSMiddleware,
+                allow_origin_regex=r"http://localhost:.*",
+                allow_methods=["GET", "POST"],
+            )
+            middleware.append(cors_middleware)
+
+        super().__init__(routes=routes, middleware=middleware, debug=debug)
