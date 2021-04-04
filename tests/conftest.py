ad8ca6b56d6c1ccab96e37c7e71e32c544cbc546
ignormies <bryce.beagle@gmail.com>
2021-04-04 09:28:54 -0700

    Add return type annotation to conftest.postgres_test_db

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 import os
+from typing import Generator
 
 import pytest
 import sqlalchemy_utils
+from playhouse.postgres_ext import PostgresqlExtDatabase
 
 from shortcake.database.management import ShortcakeDatabase
 
@@ -10,7 +12,7 @@ DATABASE_NAME = "shortcake_test"
 
 
 @pytest.fixture(autouse=True)
-def postgres_test_db():
+def postgres_test_db() -> Generator[PostgresqlExtDatabase, None, None]:
     database = ShortcakeDatabase(
         database_name=DATABASE_NAME,
         hostname=DATABASE_HOSTNAME,
