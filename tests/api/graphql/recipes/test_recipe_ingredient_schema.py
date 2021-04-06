b117ef443aac8eba6df7cc08eba3ae72bfc0439a
ignormies <bryce.beagle@gmail.com>
2021-04-05 22:58:51 -0700

    Make `RecipeIngredient.modifier` nullable in GraphQL schema

--- tests/api/graphql/recipes/test_recipe_ingredient_schema.py
+++ tests/api/graphql/recipes/test_recipe_ingredient_schema.py
@@ -4,7 +4,6 @@ from shortcake.api.graphql.recipes import RecipeIngredient
 
 
 class TestRecipeIngredientSchema:
-    @pytest.mark.xfail(reason="shortcake-dev/shortcake#54")
     def test_nullability(self):
         fields = {
             field.name: field for field in RecipeIngredient._type_definition.fields
